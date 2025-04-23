c21fa075a931f460fd4f90a214419bc7f9ed54c5
Rohit Jeerankalagi <rohitjeerankalagi07@gmail.com>
2025-04-23 08:32:30 +0530

    saved

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+
+    public void display{
+        System.out.pritnln("hi,Good Mornning");
+    }
+
+    public static viod main(String[] args){
+        test t=new test();
+        t.display();
+    }
+}
